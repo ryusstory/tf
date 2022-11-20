7f3ffff7b4c81f5507717716b795f6cfa12d73c1
ryusstory <ryusstory@Rucas-MacBookPro.local>
2022-11-20 12:49:20 +0900

    update

--- week5/200.99.tf
+++ week5/200.99.tf
@@ -4,32 +4,8 @@ variable first_number {
 }
 
 locals { 
-    # second_numbers = [for v in range(9) : tostring(v+1)] 
     second_numbers = [for v in range(9) : v+1 ] 
 }
-
-
-# resource "aws_security_group" "first_number" {
-#     name        = "first_number"
-#     description = "first_number"
-#     vpc_id      = aws_vpc.main.id
-
-#     ingress {
-#         from_port        = 1
-#         to_port          = 1
-#         protocol         = "tcp"
-#         cidr_blocks      = [aws_vpc.main.cidr_block]
-#         description      = "first_number"
-#     }
-
-#     egress {
-#         from_port        = 0
-#         to_port          = 0
-#         protocol         = "-1"
-#         cidr_blocks      = ["0.0.0.0/0"]
-#         ipv6_cidr_blocks = ["::/0"]
-#     }
-# }
 
 resource "aws_security_group" "result" {
     name        = "result"
